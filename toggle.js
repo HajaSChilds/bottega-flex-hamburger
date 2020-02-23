9ec7b927e070c57346ea0254271771bd0e30ec12
HajaSC <hajasc@protonmail.com>
2020-02-23 13:26:32 -0500

    Finished navbar and hamburger menu, added fonts and Sass

--- toggle.js
+++ toggle.js
@@ -2,10 +2,18 @@ function myFunction() {
   var x = document.getElementById("toggle");
   if (screen.width > 780) {
       x.style.display = "flex";
-    }else if (x.style.display = "none") {
+
+  }else if (x.style.display == "none" && x.screen.display == "none"){
        x.style.display = "flex";  
-  } else {
+     
+  } else  {
     x.style.display = "none";
   }
-}
 
+} 
+
+
+ /*const navHamburger = document.querySelector('#hamburger');
+    hamburger.addEventListener('click', (e) => {
+        navHamburger.parentElement.classList.toggle_btn('active');
+    }*/
